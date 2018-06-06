13412f35f8b4a22659818fcd13535b3095b471bc
leandroph <leandroph1992@hotmail.com>
2018-06-05 22:08:47 -0300

    Add mais alguns produtos

--- src/algoritmo/StartApp.java
+++ src/algoritmo/StartApp.java
@@ -24,18 +24,25 @@ public class StartApp {
         List<Produto> listaProdutos = new ArrayList<>();
         listaProdutos.add(new Produto("Geladeira Dako", 0.751, 999.90));
         listaProdutos.add(new Produto("IPhone 6", 0.0000899, 2199.12));
+        listaProdutos.add(new Produto("IPhone 6", 0.0000899, 2199.12));
         listaProdutos.add(new Produto("TV 55'", 0.400, 4346.99));
+        listaProdutos.add(new Produto("TV 50'", 0.290, 3999.90));
+        listaProdutos.add(new Produto("TV 50'", 0.290, 3999.90));
         listaProdutos.add(new Produto("TV 50'", 0.290, 3999.90));
         listaProdutos.add(new Produto("TV 42'", 0.200, 2999.90));
         listaProdutos.add(new Produto("Notebook Dell", 0.00350, 2499.90));
+        listaProdutos.add(new Produto("Ventilador Panasonic", 0.496, 199.90));
         listaProdutos.add(new Produto("Ventilador Panasonic", 0.496, 199.90));
         listaProdutos.add(new Produto("Microondas Eletrolux", 0.0424, 308.66));
         listaProdutos.add(new Produto("Microondas LG", 0.0544, 429.90));
         listaProdutos.add(new Produto("Microondas Panasonic", 0.0319, 299.29));
         listaProdutos.add(new Produto("Geladeira Brastemp", 0.635, 849.00));
         listaProdutos.add(new Produto("Geladeira Consul", 0.870, 1199.89));
+        listaProdutos.add(new Produto("Geladeira Consul", 0.870, 1199.89));
         listaProdutos.add(new Produto("Notebook Lenovo", 0.498, 1999.90));
         listaProdutos.add(new Produto("Notebook Asus", 0.527, 3999.00));
+        listaProdutos.add(new Produto("Notebook Asus", 0.527, 3999.00));
+        listaProdutos.add(new Produto("Notebook Asus", 0.527, 3999.00));//21 produtos
 
 //cria as lista para trabalhar com a classe Individuo
         List espacos = new ArrayList<>();
@@ -49,10 +56,10 @@ public class StartApp {
 
         }
 
-        Double limite = 3.0;//limite total da carga que seria de 3m³
-        int tamanhoPopulacao = 20;
-        Double taxaMutacao = 0.01;
-        int numeroGeracoes = 70;//numero total do processo de evolução
+        Double limite = 10.0;//limite total da carga que seria de 3m³
+        int tamanhoPopulacao = 40;
+        Double taxaMutacao = 0.05;
+        int numeroGeracoes = 100;//numero total do processo de evolução
 
         AlgoritmoGenetico ag = new AlgoritmoGenetico(tamanhoPopulacao);
         ag.inicializaPopulacao(espacos, valores, limite);
